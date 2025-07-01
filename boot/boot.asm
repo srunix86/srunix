b25af846d63f58e110d8d62bf5f0be1fe5959686
Srunix <kastian_kostianich2007@mail.ru>
2025-07-01 09:25:17 +0300

    Update boot.asm

--- boot/boot.asm
+++ boot/boot.asm
@@ -9,9 +9,9 @@ align 4
     dd 0x00000000
     dd 0x00000000
     dd 0x00000001
-    dd 0x00000000
-    dd 0x00000000
-    dd 0x00000000
+    dd 1920
+    dd 1080
+    dd 32
 
 section .text
 global start
@@ -34,11 +34,19 @@ global _sti
 global _hlt
 global _reboot
 global _io_wait
+global _detect_cpuid
+global _detect_long_mode
 extern kernel_main
 
 start:
     cli
     mov esp, stack_top
+    call _detect_cpuid
+    test eax, eax
+    jz .no_cpuid
+    call _detect_long_mode
+    test eax, eax
+    jz .no_long_mode
     push eax
     push ebx
     call _enable_fpu
@@ -46,9 +54,70 @@ start:
     call setup_gdt
     call setup_idt
     call setup_tss
+    mov eax, [esp + 4]
+    mov ebx, eax
+    add ebx, 48
+    push ebx
+    push dword 1920
+    push dword 1080
+    push eax
+    push dword [esp + 20]
     call kernel_main
     cli
     hlt
+
+.no_cpuid:
+    mov esi, no_cpuid_msg
+    call _print_error
+    jmp _reboot
+
+.no_long_mode:
+    mov esi, no_long_mode_msg
+    call _print_error
+    jmp _reboot
+
+_print_error:
+    mov edi, 0xB8000
+    mov ah, 0x4F
+.loop:
+    lodsb
+    test al, al
+    jz .done
+    stosw
+    jmp .loop
+.done:
+    ret
+
+_detect_cpuid:
+    pushfd
+    pop eax
+    mov ecx, eax
+    xor eax, 0x00200000
+    push eax
+    popfd
+    pushfd
+    pop eax
+    push ecx
+    popfd
+    xor eax, ecx
+    shr eax, 21
+    and eax, 1
+    ret
+
+_detect_long_mode:
+    mov eax, 0x80000000
+    cpuid
+    cmp eax, 0x80000001
+    jb .no_long_mode
+    mov eax, 0x80000001
+    cpuid
+    test edx, 1 << 29
+    jz .no_long_mode
+    mov eax, 1
+    ret
+.no_long_mode:
+    xor eax, eax
+    ret
 
 gdt_flush:
     mov eax, [esp + 4]
@@ -164,6 +233,9 @@ setup_tss:
 
 section .data
 align 4
+
+no_cpuid_msg: db "Error: CPUID not supported", 0
+no_long_mode_msg: db "Error: Long mode not supported", 0
 
 gdt_descriptor:
     dw gdt_end - gdt_start - 1

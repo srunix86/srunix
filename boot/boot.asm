ce9dbc2499f73570d60a1360076acdf5214a4ddc
Srunix <kastian_kostianich2007@mail.ru>
2025-09-02 19:52:03 +0300

    Update boot.asm

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,19 +1,25 @@
-section .multiboot
-align 4
-    dd 0x1BADB002
-    dd 0x00000003
-    dd -(0x1BADB002 + 0x00000003)
-    dd 0x00000000
-    dd 0x00000000
-    dd 0x00000000
-    dd 0x00000000
-    dd 0x00000000
-    dd 0x00000001
+section .multiboot2
+align 8
+header_start:
+    dd 0xE85250D6
+    dd 0
+    dd header_end - header_start
+    dd -(0xE85250D6 + 0 + (header_end - header_start))
+
+    dw 5
+    dw 1
+    dd 20
     dd 1920
     dd 1080
     dd 32
 
+    dw 0
+    dw 0
+    dd 8
+header_end:
+
 section .text
+bits 32
 global start
 global gdt_flush
 global idt_flush
@@ -41,42 +47,41 @@ extern kernel_main
 start:
     cli
     mov esp, stack_top
-    call _detect_cpuid
-    test eax, eax
-    jz .no_cpuid
-    call _detect_long_mode
-    test eax, eax
-    jz .no_long_mode
-    push eax
-    push ebx
+    mov dword [multiboot_info], ebx
+    
+    call check_cpu_features
+    
+    call setup_page_tables
+    call enable_paging
+    
     call _enable_fpu
     call _enable_sse
-    call setup_gdt
-    call setup_idt
-    call setup_tss
-    mov eax, [esp + 4]
-    mov ebx, eax
-    add ebx, 48
-    push ebx
-    push dword 1920
-    push dword 1080
-    push eax
-    push dword [esp + 20]
-    call kernel_main
+    
+    lgdt [gdt64.pointer]
+    jmp gdt64.code:long_mode_start
+    
     cli
     hlt
 
+check_cpu_features:
+    call _detect_cpuid
+    test eax, eax
+    jz .no_cpuid
+    
+    call _detect_long_mode
+    test eax, eax
+    jz .no_long_mode
+    
+    ret
+
 .no_cpuid:
     mov esi, no_cpuid_msg
-    call _print_error
-    jmp _reboot
+    jmp _fatal_error
 
 .no_long_mode:
     mov esi, no_long_mode_msg
-    call _print_error
-    jmp _reboot
 
-_print_error:
+_fatal_error:
     mov edi, 0xB8000
     mov ah, 0x4F
 .loop:
@@ -86,7 +91,7 @@ _print_error:
     stosw
     jmp .loop
 .done:
-    ret
+    jmp _reboot
 
 _detect_cpuid:
     pushfd
@@ -119,6 +124,78 @@ _detect_long_mode:
     xor eax, eax
     ret
 
+setup_page_tables:
+    mov eax, pdp_table
+    or eax, 0b11
+    mov [pml4_table], eax
+
+    mov eax, pd_table
+    or eax, 0b11
+    mov [pdp_table], eax
+
+    mov ecx, 0
+.map_pd_table:
+    mov eax, 0x200000
+    mul ecx
+    or eax, 0b10000011
+    mov [pd_table + ecx * 8], eax
+    
+    inc ecx
+    cmp ecx, 512
+    jne .map_pd_table
+    
+    ret
+
+enable_paging:
+    mov eax, pml4_table
+    mov cr3, eax
+    
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+    
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+    
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+    
+    ret
+
+bits 64
+long_mode_start:
+    mov ax, gdt64.data
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    
+    mov rsp, stack_top
+    
+    mov rdi, [multiboot_info]
+    call kernel_main
+    
+    cli
+    hlt
+
+section .rodata
+gdt64:
+    dq 0
+.code: equ $ - gdt64
+    dq (1<<44) | (1<<47) | (1<<41) | (1<<43) | (1<<53)
+.data: equ $ - gdt64
+    dq (1<<44) | (1<<47) | (1<<41)
+.pointer:
+    dw $ - gdt64 - 1
+    dq gdt64
+
+section .text
+bits 32
+
 gdt_flush:
     mov eax, [esp + 4]
     lgdt [eax]
@@ -145,12 +222,6 @@ tss_flush:
 load_page_directory:
     mov eax, [esp + 4]
     mov cr3, eax
-    ret
-
-enable_paging:
-    mov eax, cr0
-    or eax, 0x80000000
-    mov cr0, eax
     ret
 
 _read_cr0:
@@ -218,73 +289,22 @@ _io_wait:
     out 0x80, al
     ret
 
-setup_gdt:
-    lgdt [gdt_descriptor]
-    ret
-
-setup_idt:
-    lidt [idt_descriptor]
-    ret
-
-setup_tss:
-    mov ax, 0x2B
-    ltr ax
-    ret
-
 section .data
 align 4
 
-no_cpuid_msg: db "Error: CPUID not supported", 0
-no_long_mode_msg: db "Error: Long mode not supported", 0
-
-gdt_descriptor:
-    dw gdt_end - gdt_start - 1
-    dd gdt_start
-
-idt_descriptor:
-    dw idt_end - idt_start - 1
-    dd idt_start
-
-gdt_start:
-    dd 0x00000000
-    dd 0x00000000
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 0x9A
-    db 0xCF
-    db 0x00
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 0x92
-    db 0xCF
-    db 0x00
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 0xFA
-    db 0xCF
-    db 0x00
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 0xF2
-    db 0xCF
-    db 0x00
-    dw 0x0068
-    dw 0x0000
-    db 0x00
-    db 0x89
-    db 0x00
-    db 0x00
-gdt_end:
-
-idt_start:
-    times 256 dd 0x00000000
-idt_end:
+multiboot_info: dd 0
+no_cpuid_msg: db "CPUID not supported - System halted", 0
+no_long_mode_msg: db "64-bit mode not supported - System halted", 0
 
 section .bss
+align 4096
+pml4_table:
+    resb 4096
+pdp_table:
+    resb 4096
+pd_table:
+    resb 4096
+
 align 16
 stack_bottom:
     resb 16384
